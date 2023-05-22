8804d878b9822a86c01f431032db51ccfcd32b43
Everton Buzzi <evertonbuzzi@gmail.com>
2023-05-22 09:17:38 +0100

    #EB: Adding GraphQL to crwn-clothing

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,14 +1,31 @@
 import { createContext, useState, useEffect } from 'react';
-
+import { gql, useQuery } from '@apollo/client';
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS)
   const [categoriesMap, setCategoriesMap] = useState({});
 
+  console.log(data, loading, error);
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
